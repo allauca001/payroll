dd4f8266c7c0c0323465859c3cc1d0c763d9b9f5
allauca001 <oallauca@yahoo.com>
2013-08-12 14:02:23 -0400

    tax-pay function

--- payroll.rb
+++ payroll.rb
@@ -1,10 +1,16 @@
 class Payroll
-	def base_pay(hourly_rate, hours)
+
+	def self.base_pay(hourly_rate, hours)
 		hourly_rate * hours
 	end
 
-	def gross_pay(hourly_rate, hours)
+	def self.gross_pay(hourly_rate, hours)
 		total_hours = hours.inject { |sum, x| sum + x }
 		base_pay(hourly_rate, total_hours)
 	end
+
+	def self.tax_pay(gross_pay, tax_rate)
+		gross_pay * tax_rate
+	end
+
 end
--- tests/payroll_spec.rb
+++ tests/payroll_spec.rb
@@ -3,15 +3,21 @@ require_relative '../payroll.rb'
 describe Payroll do
 	
 	it "should calculate base pay" do
-		payroll = Payroll.new
-		basepay = payroll.base_pay hourly_rate=80, hours=20
+		#payroll = Payroll.new
+		basepay = Payroll.base_pay hourly_rate=80, hours=20
 		1600.should be_equal basepay
 	end
 
 	it "should calculate gross pay" do
-		payroll = Payroll.new
-		grosspay = payroll.gross_pay hourly_rate=80, hours=[8,8,10,6]
+		#payroll = Payroll.new
+		grosspay = Payroll.gross_pay hourly_rate=80, hours=[8,8,10,6]
 		2560.should be_equal grosspay
+	end
+
+	it "should calculate Federal income taxes" do
+		#payroll = Payroll.new
+		taxpay = Payroll.tax_pay gross_pay=5000, tax_rate=0.25
+		(1250.0).should be_equal taxpay
 	end
 	
 end
